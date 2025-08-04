4ca259646c8fdf7b386963bd0724f2d5a4371eae
varias76 <111916290+varias76@users.noreply.github.com>
2025-08-04 14:53:57 -0400

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+    }
+}
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,5 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ChatApp"
+rootProject.name = "chatapp"
 include(":app")
+ 
